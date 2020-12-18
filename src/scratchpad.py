59f07008dd4ddef715bb19ec2923128f754785a5
Tom Tarpey <tomtarpeydev@gmail.com>
2020-12-18 01:24:09 +0000

    added ht stuff in scratchpad

---
+++ src/scratchpad.py
@@ -0,0 +1,116 @@
+"""
+1. Write the delete method with the assumption that linked list chaining was used for collision resolution.
+2. Write the get method with the assumption that linked list chaining was used for collision resolution.
+"""
+class HashTableEntry:
+    """
+    Linked List hash table key/value pair
+    """
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+
+# Hash table can't have fewer than this many slots
+MIN_CAPACITY = 8
+
+
+class HashTable:
+    """
+    A hash table that with `capacity` buckets
+    that accepts string keys
+    Implement this.
+    """
+
+    def __init__(self, capacity):
+        self.capacity = capacity  # Number of buckets in the hash table
+        self.storage = [None] * capacity
+        self.item_count = 0
+
+
+    def get_num_slots(self):
+        """
+        Return the length of the list you're using to hold the hash
+        table data. (Not the number of items stored in the hash table,
+        but the number of slots in the main list.)
+        One of the tests relies on this.
+        Implement this.
+        """
+        # Your code here
+
+
+    def get_load_factor(self):
+        """
+        Return the load factor for this hash table.
+        Implement this.
+        """
+        return len(self.storage)
+
+
+    def djb2(self, key):
+        """
+        DJB2 hash, 32-bit
+        Implement this, and/or FNV-1.
+        """
+        
+        # Cast the key to a string and get bytes
+        str_key = str(key).encode()
+
+        # Start from an arbitrary large prime
+        hash_value = 5381
+
+        # Bit-shift and sum value for each character
+        for b in str_key:
+            hash_value = ((hash_value << 5) + hash_value) + b
+            hash_value &= 0xffffffff  # DJB2 is a 32-bit hash, only keep 32 bits
+
+        return hash_value
+
+
+    def hash_index(self, key):
+        """
+        Take an arbitrary key and return a valid integer index
+        between within the storage capacity of the hash table.
+        """
+        return self.djb2(key) % self.capacity
+
+    def put(self, key, value):
+        """
+        Store the value with the given key.
+        Hash collisions should be handled with Linked List Chaining.
+        Implement this.
+        """
+        index = self.hash_index(key)
+
+        current_entry = self.storage[index]
+
+        while current_entry is not None and current_entry.key != key:
+            current_entry = current_entry.next
+
+        if current_entry is not None:
+            current_entry.value = value
+        else:
+            new_entry = HashTableEntry(key, value)
+            new_entry.next = self.storage[index]
+            self.storage[index] = new_entry
+        
+        # TODO: handle resize?
+
+
+    def delete(self, key):
+        """
+        Remove the value stored with the given key.
+        Print a warning if the key is not found.
+        Implement this.
+        """
+        # Your code here
+
+
+    def get(self, key):
+        """
+        Retrieve the value stored with the given key.
+        Returns None if the key is not found.
+        Implement this.
+        """
+        # Your code here
