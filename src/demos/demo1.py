278cebb97ed1d8aa9f473936e60dbf8772eccd4f
Tom Tarpey <tomtarpeydev@gmail.com>
2020-12-18 00:39:15 +0000

    initial code

---
+++ src/demos/demo1.py
@@ -0,0 +1,42 @@
+"""
+You are given a non-empty list of words.
+Write a function that returns the *k* most frequent elements.
+The list that you return should be sorted by frequency from highest to lowest.
+If two words have the same frequency, then the word with the lower alphabetical
+order should come first.
+Example 1:
+```plaintext
+Input:
+words = ["lambda", "school", "rules", "lambda", "school", "rocks"]
+k = 2
+Output:
+["lambda", "school"]
+Explanation:
+"lambda" and "school" are the two most frequent words.
+```
+Example 2:
+```plaintext
+Input:
+words = ["the", "sky", "is", "cloudy", "the", "the", "the", "cloudy", "is", "is"]
+k = 4
+Output:
+["the", "is", "cloudy", "sky"]
+Explanation:
+"the", "is", "cloudy", and "sky" are the four most frequent words. The words
+are sorted from highest frequency to lowest.
+```
+Notes:
+- `k` is always valid: `1 <= k <= number of unique elements.
+- words in the input list only contain lowercase letters.
+```
+"""
+def top_k_frequent(words, k):
+    """
+    Input:
+    words -> List[str]
+    k -> int
+    Output:
+    List[str]
+    """
+    # Your code here
+    pass
---
+++ src/demos/demo2.py
@@ -0,0 +1,42 @@
+"""
+Given a string, sort it in decreasing order based on the frequency of characters.
+Example 1:
+```plaintext
+Input:
+"free"
+Output:
+"eefr"
+Explanation:
+'e' appears twice while 'f' and 'r' appear once.
+So 'e' must appear before 'f' and 'r'. Therefore, "eerf" is also a valid answer.
+```
+Example 2:
+```plaintext
+Input:
+"dddbbb"
+Output:
+"dddbbb"
+Explanation:
+Both 'd' and 'b' appear three times, so "bbbddd" is also a valid answer.
+Note that "dbdbdb" is incorrect, as the same characters must be together.
+```
+Example 3:
+```plaintext
+Input:
+"Bbcc"
+Output:
+"ccBb"
+Explanation:
+"ccbB" is also a valid answer, but "Bbcc" is incorrect.
+Note that 'B' and 'b' are treated as two different characters.
+```
+"""
+def frequency_sort(s: str) -> str:
+    """
+    Inputs:
+    s -> str
+    Output:
+    str
+    """
+    # Your code here
+    pass
